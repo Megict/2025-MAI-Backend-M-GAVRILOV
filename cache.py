544953d88f12d8b9caa0fb350ba4d481c6f0bf6b
megict <cobraj@yandex.ru>
2025-04-09 02:19:17 +0300

    cache thing

---
+++ cache.py
@@ -0,0 +1,24 @@
+class LRUCache:
+    def __init__(self, capacity: int=10) -> None:
+        self.cache_dict = {}
+        self.capacity = capacity
+
+    def get(self, key: str) -> str:
+        try:
+            return self.cache_dict[key]
+        except KeyError:
+            return ''
+
+    def set(self, key: str, value: str) -> None:
+        if key in self.cache_dict: # чтобы неявно хранить порядок использования в структуре словаря
+            self.cache_dict.pop(key)
+        self.cache_dict[key] = value
+        if len(self.cache_dict) > self.capacity:
+            self.cache_dict = dict(list(self.cache_dict.items())[-self.capacity : ])
+
+
+    def rem(self, key: str) -> None:
+        try:
+            self.cache_dict.pop(key)
+        except KeyError:
+            pass
